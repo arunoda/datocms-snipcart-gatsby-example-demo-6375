0d3ae7ffe9960bafa978b525e8267070075b43f5
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2020-02-18 23:51:08 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Snipcart + GatsbyJS + DatoCMS example
+
+## Deploy on DatoCMS
+
+[![Deploy with DatoCMS](https://dashboard.datocms.com/deploy/button.svg)](https://dashboard.datocms.com/deploy?repo=datocms/snipcart-gatsby-demo)
---
+++ gatsby-config.js
@@ -0,0 +1,24 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
+module.exports = {
+  siteMetadata: {
+    siteName: 'My Shop',
+  },
+  plugins: [
+    'gatsby-plugin-sass',
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-source-datocms`,
+      options: { apiToken: process.env.DATO_API_TOKEN },
+    },
+    {
+      resolve: 'gatsby-plugin-snipcart',
+      options: {
+        apiKey: 'OWE3MmZmMjQtNTk3Yi00OThhLWEwMmUtZDY4ZWM4NzIwYzZiNjM2NjM0Mzc1NzE0MTUwNzI1',
+        autopop: true
+      }
+    },
+  ],
+}
